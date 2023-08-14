6ecdd9dc7dbab19b7ac044d2d41906589c69ced3
Abdullah Imran <abdullahimran999@gmail.com>
2023-08-15 01:56:21 +0500

    Added some basic Functionalities

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-function white(){
-    body.className='white';
+const allsections = document.querySelector('.main-content');
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+
+function PageTranition() {
+    // change the active button css
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function () {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        });
+    }
+
+    // change of section 
+    allsections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove selected from other buttons
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+
+    })
+
 }
 
-function dark(){
-    body.className='dark';
+PageTranition();
+
+
+function white() {
+    body.className = 'white';
+}
+
+function dark() {
+    body.className = 'dark';
 }
