f74957c1d28030460834aafcc5815e7e5a70af8c
Abdullah Imran <abdullahimran999@gmail.com>
2023-08-18 01:49:15 +0500

    added major code

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-const allsections = document.querySelector('.main-content');
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-
-function PageTranition() {
-    // change the active button css
-    for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function () {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        });
-    }
-
-    // change of section 
-    allsections.addEventListener('click', (e) => {
-        const id = e.target.dataset.id;
-        if (id) {
-            // remove selected from other buttons
-            sectBtns.forEach((btn)=>{
-                btn.classList.remove('active');
-            })
-            e.target.classList.add('active');
-
-            // hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active');
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
-
-}
-
-PageTranition();
-
-
-function white() {
-    body.className = 'white';
-}
-
-function dark() {
-    body.className = 'dark';
-}
+})();
